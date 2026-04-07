83698ac3f36a6f9a58c370afad756ae2c8307802
Franck Daniel <francknguedjang@gmail.com>
2026-04-07 04:34:02 +0100

    Fix case sensitivity for AuthController import in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Authcontroller;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DepotController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\ReportController;
